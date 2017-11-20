7087308f64afbec453c2b126a4288c651cc72367
Crista Cooper <aine3d@gmail.com>
2017-11-20 02:45:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,9 @@ def prime?(integer)
 
   is_prime = true
   return false if integer <= 1
-  
+
   for i in 2..integer - 1
-    if integer % i == 0
+    if integer % i = 0
       is_prime == false
   end
 end
