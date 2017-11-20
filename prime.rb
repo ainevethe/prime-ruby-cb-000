676b5a67f50092fb5e4882853476dcbc07ba38d0
Crista Cooper <aine3d@gmail.com>
2017-11-20 02:42:16 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -8,7 +8,8 @@ def prime?(integer)
   for i in 2..integer - 1
     if integer % i == 0
       is_prime = return false
+      elsif integer <= 1
+        return false
     end
-    return false if integer <= 1
   end
 end
